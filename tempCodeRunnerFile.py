cf22f9bf5fb01f08350a6a5c0bfdf5859bf06c27
douglas-hjm <168352933+douglas-hjm@users.noreply.github.com>
2024-04-28 22:35:54 +0800

    del_som—code

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-try:
-    req = HTTPSConnection(host)
-    req.request("POST", "/", headers=headers, body=payload.encode("utf-8"))
-    resp = req.getresponse()
-    print(resp.read())
-except Exception as err:
-    print(err)
