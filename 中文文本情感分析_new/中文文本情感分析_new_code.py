2e23269808e38bfe1b5e6a37c1ca590ae3aeda2b
surui <1923747154@qq.com>
2024-05-01 21:14:30 +0800

    little-change

--- 中文文本情感分析_new/中文文本情感分析_new_code.py
+++ 中文文本情感分析_new/中文文本情感分析_new_code.py
@@ -1,10 +1,31 @@
 
 import numpy as np
 import pandas as pd
+import pretty_errors
+pretty_errors.activate()
+
+'''
+先改这个！！！
+'''
+############################################
+user1 = "surui"
+############################################
+
+if(user1=="renbaoqi"):
+    ori_data=r"C:\Users\18356\Desktop\大一年度项目资料\中文文本情感分析_new\ori data.xlsx"
+    stop_words_file = r"C:\Users\18356\Desktop\大一年度项目资料\中文文本情感分析_new\哈工大停用词表.txt"
+    data=r"C:\Users\18356\Desktop\大一年度项目资料\中文文本情感分析_new\data.xlsx"
+elif(user1=="surui"):
+    ori_data=r"/Users/surui/Library/Containers/com.tencent.xinWeChat/Data/Library/Application Support/com.tencent.xinWeChat/2.0b4.0.9/cfa43d013fb378e45355d09126bc529f/Message/MessageTemp/bd32bd434566b9ad7bb1db6ed50133bf/File/ori data.xlsx"
+    stop_words_file = r"中文文本情感分析_new/哈工大停用词表.txt"
+    data=r"data.xlsx"
+elif(user1=="hujiaming"):
+    ori_data=r""
+    stop_words_file = r""
+    data=r"" 
 
 
-
-data = pd.read_excel(r"C:\Users\18356\Desktop\大一年度项目资料\中文文本情感分析_new\ori data.xlsx").astype(str)
+data = pd.read_excel(ori_data).astype(str)
 data.head()
 
 
@@ -18,10 +39,12 @@ import jieba
 def chinese_word_cut(mytext):
     return" ".join(jieba.cut(mytext))
 
-data['cut_comment'] = data.comment.apply(chinese_word_cut)
-data.head()
 
-
+    
+    
+#更改一下，只让他分第一列--surui，将jieba分词的结果保存到data['cut_comment']中
+#data['cut_comment'] = data.comment.apply(chinese_word_cut)
+data['cut_comment'] = data.iloc[:, 0].apply(chinese_word_cut)
 data.head()
 
 
@@ -37,7 +60,7 @@ def get_custom_stopwords(stop_words_file):
     custom_stopwords_list = [i for i in stopwords_list]
     return custom_stopwords_list
 
-stop_words_file = r"C:\Users\18356\Desktop\大一年度项目资料\中文文本情感分析_new\哈工大停用词表.txt"
+
 stopwords = get_custom_stopwords(stop_words_file)
 
 vect = CountVectorizer(max_df = 0.8, 
@@ -72,6 +95,9 @@ y_train_cleaned = y_train[y_train.notnull()]
 X_train_vect = vect.fit_transform(X_train_cleaned)
 nb.fit(X_train_vect, y_train_cleaned)
 
+# 保存模形--- 没有，先不保存
+import joblib
+joblib.dump(nb, 'model.pkl')
 
 # #### 评估模型
 train_score = nb.score(X_train_vect, y_train_cleaned)
@@ -111,48 +137,55 @@ import matplotlib.pyplot as plt
 
 # 计算真值
 y_test_true = y_test
-print(y_test_true)
+#打印什么真值，不打印，闹心
+#print(y_test_true)
 # 计算预测值
 y_test_pred = nb.predict_proba(X_test_vect)[:,1]
 
 # 计算ROC曲线
-fpr, tpr, thresholds = roc_curve(y_test_true, y_test_pred)
-roc_auc = auc(fpr, tpr)
+# 切换字符串为数字--先不整
+y_test_true = y_test_true.replace({'1': 1, '0': 0})
+#fpr, tpr, thresholds = roc_curve(y_test_true, y_test_pred)
+#roc_auc = auc(fpr, tpr)
 
-# 绘制ROC曲线
-plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
-plt.legend(loc="lower right")
-plt.show()
+# 绘制ROC曲线---不画
+def draw():
+    plt.figure()
+    #plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='-')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+    plt.show()
 
+#draw()
 
 
 
 # #### 分析数据 
 
 
-data = pd.read_excel(r"C:\Users\18356\Desktop\大一年度项目资料\中文文本情感分析_new\data.xlsx")
-data.head()
+def ana():
+    data = pd.read_excel(data)
+    data.head()
 
-data = pd.read_excel("data.xlsx")
-data['cut_comment'] = data.comment.apply(chinese_word_cut)
-X=data['cut_comment']
-X_vec = vect.transform(X)
-nb_result = nb.predict(X_vec)
-#predict_proba(X)[source] 返回概率
-data['nb_result'] = nb_result
+    data = pd.read_excel("data.xlsx")
+    #data['cut_comment'] = data.comment.apply(chinese_word_cut)
+    X=data['cut_comment']
+    X_vec = vect.transform(X)
+    nb_result = nb.predict(X_vec)
+    #predict_proba(X)[source] 返回概率
+    data['nb_result'] = nb_result
 
 
-test = pd.DataFrame(vect.fit_transform(X).toarray(), columns=vect.get_feature_names_out())
-test.head()
+    test = pd.DataFrame(vect.fit_transform(X).toarray(), columns=vect.get_feature_names_out())
+    test.head()
 
-data.to_excel("data_result.xlsx",index=False)
-
+    data.to_excel("data_result.xlsx",index=False)
+#先不分析
+#ana()
 
 
